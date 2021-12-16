e59463845e1a0c98b8ccb2fb481aba534481f5ae
Andrej Lotski <lotskiandrej@gmail.com>
2021-12-17 00:20:37 +0100

    Initial Commit

---
+++ Swift Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Movies
+//
+//  Created by Andrej Lotski on 17/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
